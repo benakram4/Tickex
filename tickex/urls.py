7f5fef0e384d9478f39450961b461eb0e4414ca0
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-03-18 01:37:06 -0400

    .

--- tickex/urls.py
+++ tickex/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
@@ -24,3 +25,6 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('tickets/', include('tickets.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
