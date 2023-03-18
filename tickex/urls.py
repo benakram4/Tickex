50785939968f86ff62863655c4370c3ef2ee3c9b
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-03-18 01:49:44 -0400

    1.2

--- tickex/urls.py
+++ tickex/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('tickets/', include('tickets.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
