56e052827395ca8d87c4d497909eaae5383d8384
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-03-18 01:18:56 -0400

    dep

--- tickex/settings.py
+++ tickex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fno2822cv3^@*0%dm%s8t6j8^piq_j@(&ozg^#x=@xdgn(p96b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', "127.0.0.1"]
 
 
 # Application definition
