9a1adced74d8811acfdf3d7371ba29f3837e68c7
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-03-21 13:20:06 -0400

    integrated crispy forms

--- tickex/settings.py
+++ tickex/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #django crispy forms
+    "crispy_forms",
+    "crispy_bootstrap5",
+    
     # My Apps
     'tickex',
     'events',
@@ -47,6 +51,11 @@ INSTALLED_APPS = [
     'users',
     'venues',
 ]
+
+# Crispy Forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
